70a8080f77d7225a0902efd2410910aea4948ee4
ibadzadeee <sara.ibadzade@gmail.com>
2023-11-23 16:26:18 +0400

    11.23.2023

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-let btn = document.querySelector("#create")
-let section = document.querySelector(".section")
-btn.addEventListener("click" , ()=>{
-    let div = document.createElement("div")
-    section.append(div)
-    div.style.width = "200px"
-    div.style.height = "200px"
-    div.style.background = "red"
-})
+// let btn = document.querySelector("#create")
+// let section = document.querySelector(".section")
+// btn.addEventListener("click" , ()=>{
+//     let div = document.createElement("div")
+//     section.append(div)
+//     div.style.width = "200px"
+//     div.style.height = "200px"
+//     div.style.background = "red"
+// })
 
-let arr = [{
-    ad : "Xedice , Samir" ,
-    yas : "19 , 22",
-}]
 
-let ul = document.querySelector("ul")
-let li = document.createElement("li")
-li.add = arr
-console.log(li)
-ul.append(li)
+// -----------------------------------------2--------------------------------------------
+
+// let arr = [
+//     {name : "Xedice" , age : "19"},
+//     {name :  "Samir" , age : "22"}
+// ]
+
+
+// let btn_filter = document.querySelector("#filter")
+// let ul = document.querySelector("ul")
+
+// for( let i = 0 ; i<=arr.length; i++){
+//     let li = document.createElement("li")
+//     li.innerText += arr[i].name + "-" + arr[i].age
+//     ul.append(li)
+// }
+
+// btn_filter.addEventListener("click" , ()=>{
+//     let a = arr.filter(item =>)
+// })
+
+
+
+
+// -----------------------------------------3--------------------------------------------
+
+// let btn = document.querySelector("#full_name")
+// let nameInput = document.querySelector("#name")
+// let surname = document.querySelector("#surname")
+// let h1 = document.querySelector("h1")
+
+// btn.addEventListener("click" , ()=>{
+//     h1.innerText = nameInput.value + "--"+ surname.value
+//     h1.append(body)
+// })
+
+
+// -----------------------------------------4--------------------------------------------
+
+let nav = document.querySelector("nav")
+let nextSibling = nav.firstElementChild.nextElementSibling.firstElementChild;
+console.log(nextSibling);
+let div = nav.firstElementChild.nextElementSibling.nextElementSibling;
+console.log(div);
+
+// let spanThree = 
+
+
+
+
+
 
